49d2d5c9b60c736c0526ac86e048819ea18d0723
Avesh <kadriavesh47@gmail.com>
2023-10-19 16:50:11 +0530

    Portfolio live on netlify

---
+++ src/Logins.js
@@ -0,0 +1,14 @@
+// src/Login.js
+
+import React from 'react';
+
+function Logins() {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      {/* Add your login form here */}
+    </div>
+  );
+}
+
+export default Logins;
---
+++ src/data.js
@@ -0,0 +1,95 @@
+// src/data.js
+
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+  export const skills = [
+    {
+      title: "JavaScript",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      icon: "javascript-icon.png", // You can provide the path to an image or icon here
+    },
+    {
+      title: "React",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      icon: "react-icon.png", // You can provide the path to an image or icon here
+    },
+    {
+      title: "Node.js",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      icon: "nodejs-icon.png", // You can provide the path to an image or icon here
+    },
+    {
+      title: "HTML/CSS",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      icon: "html-css-icon.png", // You can provide the path to an image or icon here
+    },
+    // Add more skills as needed
+  ];
+
+  export const testimonials = [
+    {
+      name: "John Doe",
+      title: "Web Developer",
+      company: "ABC Company",
+      image: "javascript-icon.png", // You can provide the path to an image or icon here
+      quote:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut pulvinar mi. Nullam dignissim accumsan orci at malesuada. In fringilla lectus.",
+    },
+    {
+      name: "Jane Smith",
+      title: "Frontend Developer",
+      company: "XYZ Corporation",
+      image: "react-icon.png", // You can provide the path to an image or icon here
+      quote:
+        "Praesent elementum fermentum neque, a lacinia justo euismod eu. Aenean suscipit ullamcorper tortor, eu tincidunt purus venenatis vel. Sed sollicitudin.",
+    },
+    {
+      name: "Bob Johnson",
+      title: "UI/UX Designer",
+      company: "DEF Design Studio",
+      image: "nodejs-icon.png", // You can provide the path to an image or icon here
+      quote:
+        "Suspendisse semper purus nec metus rhoncus, sit amet sagittis neque sollicitudin. Sed auctor, lorem nec fermentum molestie, lectus.",
+    },
+    {
+      name: "Alice Wilson",
+      title: "Full Stack Developer",
+      company: "GHI Tech",
+      image: "html-css-icon.png", // You can provide the path to an image or icon here
+      quote:
+        "Aenean vehicula purus eu libero condimentum, sit amet hendrerit nisl faucibus. Integer sodales ex vel hendrerit laoreet. Phasellus eget mi.",
+    },
+  ];
